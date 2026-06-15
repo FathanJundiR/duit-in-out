8a72e36fa3c6aff2b4c37272c4f7c0fc94852d32
FathanJundiR <fathanjundi43@gmail.com>
2026-06-15 20:02:37 +0700

    feat: add bottom navigation

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,6 +1,34 @@
 import { Tabs } from "expo-router";
-import { AntDesign } from "@react-native-vector-icons/ant-design/static";
+import { Ionicons } from "@react-native-vector-icons/ionicons";
 
 export default function TabLayout() {
-  return <Tabs></Tabs>;
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: "#ffd33d",
+        headerStyle: {
+          backgroundColor: "#25292e",
+        },
+        headerShadowVisible: false,
+        headerTintColor: "#fff", //warna huruf di header
+        tabBarStyle: {
+          backgroundColor: "#25292e",
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={focused ? "home" : "home-outline"}
+              color={color}
+              size={24}
+            />
+          ),
+        }}
+      />
+    </Tabs>
+  );
 }
